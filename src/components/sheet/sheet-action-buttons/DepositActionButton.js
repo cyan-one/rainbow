50c9b247d4be17c82dd29bc3eca06a29a660188e
Michael Demarais <m.demarais@gmail.com>
2020-06-15 09:08:10 -0400

    🆕️ create LiquidityPoolExpandedState screen

---
+++ src/components/sheet/sheet-action-buttons/DepositActionButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { colors } from '../../../styles';
+import { neverRerender } from '../../../utils';
+import {
+  FloatingEmojis,
+  FloatingEmojisTapHandler,
+} from '../../floating-emojis';
+import SheetActionButton from './SheetActionButton';
+
+const emojis = ['soon', 'soon', 'soon', 'soon', 'unicorn', 'soon', 'rainbow'];
+
+const DepositActionButton = ({ color = colors.dark, ...props }) => (
+  <FloatingEmojis
+    distance={350}
+    duration={2000}
+    emojis={emojis}
+    wiggleFactor={1}
+  >
+    {({ onNewEmoji }) => (
+      <FloatingEmojisTapHandler onNewEmoji={onNewEmoji}>
+        <SheetActionButton {...props} color={color} label="􀁍 Deposit" />
+      </FloatingEmojisTapHandler>
+    )}
+  </FloatingEmojis>
+);
+
+export default neverRerender(DepositActionButton);
